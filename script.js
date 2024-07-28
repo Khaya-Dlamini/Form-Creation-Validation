da2793e9f9f700385f3ecc2c81920c50d954a33e
Khayelihle <khayadlamini563@gmail.com>
2024-07-28 18:41:32 +0200

    with fuction

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('registration-form');
-    const feedbackDiv = document.getElementById('form-feedback');
+document.addEventListener('DOMContentLoaded', function() {
+    var form = document.getElementById('registration-form');
+    var feedbackDiv = document.getElementById('form-feedback');
 
-    form.addEventListener('submit', (event) => {
+    form.addEventListener('submit', function(event) {
         event.preventDefault();
-
         const username = document.getElementById('username').value.trim();
         const email = document.getElementById('email').value.trim();
         const password = document.getElementById('password').value.trim();
